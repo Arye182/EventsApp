d7c9d075b80c45a1d3ba784e42912f81c8c3842d
Arye Amsalem <arye.amsalem@gmail.com>
2022-01-11 18:51:20 +0200

    can save new events now

--- app/src/main/java/com/example/eventsapp/ui/events/EventsFragment.kt
+++ app/src/main/java/com/example/eventsapp/ui/events/EventsFragment.kt
@@ -1,8 +1,13 @@
 package com.example.eventsapp.ui.events
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.eventsapp.R
+import com.example.eventsapp.databinding.FragmentEventsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,5 +15,36 @@ class EventsFragment : Fragment(R.layout.fragment_events) {
 
     private val viewModel: EventsViewModel by viewModels()
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentEventsBinding.bind(view)
+
+        val eventAdapter = EventsAdapter()
+
+        binding.apply {
+            // recycler view
+            eventsRecyclerView.apply{
+                adapter = eventAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                setHasFixedSize(true)
+            }
+
+            // new event edit text
+            newEventEditText.addTextChangedListener {
+                viewModel.eventName = it.toString()
+            }
+
+
+            // save button
+            buttonSaveEvent.setOnClickListener{
+                viewModel.onSaveClick()
+            }
+        }
+
+        viewModel.events.observe(viewLifecycleOwner){
+            eventAdapter.submitList(it)
+        }
+    }
 
 }
--- app/src/main/java/com/example/eventsapp/ui/events/EventsViewModel.kt
+++ app/src/main/java/com/example/eventsapp/ui/events/EventsViewModel.kt
@@ -1,11 +1,50 @@
 package com.example.eventsapp.ui.events
 
+import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.example.eventsapp.data.EventDao
+import com.example.eventsapp.data.EventEntity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class EventsViewModel @ViewModelInject constructor(
-    private val eventDao: EventDao
-) : ViewModel(){
+    private val eventDao: EventDao,
+    @Assisted private val state: SavedStateHandle
+) : ViewModel() {
 
+    // getting list of events from db
+    val events = eventDao.getEvents().asLiveData()
+
+    // current event to be saved
+    val event = state.get<EventEntity>("event")
+
+    // event to be saved name
+    var eventName = state.get<String>("eventName") ?: event?.name ?: ""
+        set(value) {
+            field = value
+            state.set("eventName", value)
+        }
+
+    // save button on click
+    fun onSaveClick() {
+        if (eventName.isBlank()) {
+            // show invalid input
+        }
+
+        val newEvent = EventEntity(name = eventName)
+        createEvent(newEvent)
+
+    }
+
+    private fun createEvent(event : EventEntity) = viewModelScope.launch(Dispatchers.IO) {
+        eventDao.save(event)
+    }
+
+    private fun deleteEvent(event: EventEntity) = viewModelScope.launch(Dispatchers.IO){
+
+    }
 }
